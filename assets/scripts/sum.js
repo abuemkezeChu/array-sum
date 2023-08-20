1907ac65faf3a8ae6e196c68d15ce56648dba6fe
Abuemkeze Chu <abuemkeze@gmail.com>
2023-08-20 13:25:30 +0100

    fix: lint errors

--- assets/scripts/sum.js
+++ assets/scripts/sum.js
@@ -1,25 +1,25 @@
-'use strict';
+'use strict'
 
-const btn = document.getElementById('calculate-btn');
-const display = document.getElementById('output');
+const btn = document.getElementById('calculate-btn')
+const display = document.getElementById('output')
 
 // Function to sum the elements in the array
 const sumElements = values => {
-  let sum = 0;
+  let sum = 0
   for (const value of values) {
-    sum += value;
+    sum += value
   }
-  return sum;
-};
+  return sum
+}
 
 // Function to display the sum
 const displayOutput = () => {
-  const input = document.querySelector('#input');
+  const input = document.querySelector('#input')
   // Convert all substrings from the input field into numbers 
-  const values = input.value.split(',').map(Number);
-  const result = sumElements(values);
-  display.textContent = `Sum: ${result}`;
+  const values = input.value.split(',').map(Number)
+  const result = sumElements(values)
+  display.textContent = `Sum: ${result}`
   display.style.color = '#fff'
-};
+}
 
-btn.addEventListener('click', displayOutput);
+btn.addEventListener('click', displayOutput)
